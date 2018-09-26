75553716743d9d534b511bf54211312f46e0b967
Olivia Kasten <odkasten@gmail.com>
2018-09-26 20:36:46 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -17,6 +17,8 @@ def run_guessing_game
       puts "Guess a number between 1 and 6: "
       guess = gets.chomp
     end 
-   end 
-   puts "Goodbye"
+   end
+  if guess == "exit"
+    puts "Goodbye"
+  end 
 end 
