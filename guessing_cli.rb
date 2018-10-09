d01ccadcb9149e87f0c1e2658d77ab321ba6ae18
github username <github email address>
2018-10-08 22:01:48 -0400

    mild change to game, still working

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,18 +5,18 @@ def run_guessing_game
 
   puts "Guess a number between 1 and 6: "
   guess = gets.chomp
-  
+
   while guess != "exit"
     if guess.to_i > 0 && guess.to_i < 7
-      if number == guess.to_i 
+      if number == guess.to_i
         puts "You guessed the correct number!"
-      else 
+      else
         puts "The computer guessed #{number}."
       end
+    else
       puts "Goodbye!"
-      break
     end
-   end 
- 
+  end
+
   puts "Goodbye!"
-end 
+end
